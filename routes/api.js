562c6496a34ef266ed0cb4783f3d0900d89cc2b3
bmerkle3 <braidymerkle@gmail.com>
2017-09-19 22:28:37 -0500

    add fail and confirmation notice in api.js

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var zoneController = require('../controllers/zoneController');
 
 router.get('/:resource', function(req, res, next) {
   var resource = req.params.resource;
 
-  res.json({
-    confirmation: 'success',
-    resource: resource
-  });
+  if (resource == 'zone') {
+    zoneController.find(req.query, function(err, results) {
+      if (err) {
+        res.json({
+          confirmation: 'fail',
+          message: err
+        });
+        return;
+      }
+
+      res.json({
+        confirmation: 'success',
+        results: results
+      });
+    });
+  }
+
+  // res.json({
+  //   confirmation: 'success',
+  //   resource: resource
+  // });
 });
 
 module.exports = router;
